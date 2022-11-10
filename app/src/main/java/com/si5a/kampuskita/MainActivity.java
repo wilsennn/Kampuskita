90b8f9396c7659ad0c8b7a07a693bae89820f373
wilsennn <wilsenfendrius@mhs.mdp.ac.id>
2022-11-10 12:48:37 +0700

    kampus kita
    11/10/2022

--- app/src/main/java/com/si5a/kampuskita/MainActivity.java
+++ app/src/main/java/com/si5a/kampuskita/MainActivity.java
@@ -1,16 +1,57 @@
 package com.si5a.kampuskita;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView bnvKampusKita;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bnvKampusKita = findViewById(R.id.bnv_kampus_kita);
+
+        bukaFragment(new homeFragment());
+        getSupportActionBar().setTitle("Home");
+
+        bnvKampusKita.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.menu_home:
+                        bukaFragment(new homeFragment());
+                        getSupportActionBar().setTitle("Home");
+                        return true;
+                    case R.id.menu_jadwal:
+                        bukaFragment(new jadwalFragment());
+                        getSupportActionBar().setTitle("Jadwal");
+                        return true;
+                    case R.id.menu_nilai:
+                        bukaFragment(new nilaiFragment());
+                        getSupportActionBar().setTitle("Nilai");
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+    private void bukaFragment(Fragment varFragment){
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.fl_container, varFragment);
+        ft.commit();
     }
 
 
