cc0bd0e128852c38db95155b627ed12d8f0e9ef3
wilsennn <wilsenfendrius@mhs.mdp.ac.id>
2022-11-10 12:29:40 +0700

    kampus kita
    11/10/2022

---
+++ app/src/main/java/com/si5a/kampuskita/MainActivity.java
@@ -0,0 +1,22 @@
+package com.si5a.kampuskita;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_atas, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
